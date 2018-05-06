d6ada25b00c9fd9b4711894c1a3d5fb0fb2c800f
George Stone <westborn@users.noreply.github.com>
2018-05-06 21:48:18 +1000

    Fisrt WESTBORN Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default class Products extends React.Component {
         return netlifyIdentity.currentUser() != null
             ? this.props.data.allMarkdownRemark.edges
             : this.props.data.allMarkdownRemark.edges
-                .filter(x => !x.node.frontmatter.private)  
+                .filter(x => !x.node.frontmatter.private)
     }
 
     updateProducts(){
@@ -32,7 +32,7 @@ export default class Products extends React.Component {
     render(){
         return (
         <div>
-            <h1>Products</h1>
+            <h1>WESTBORN Products</h1>
             <div>To login use the email: geeks@snipcart.com with password: admin</div>
             <div>You can read the full blog post <a href="https://snipcart.com/blog/static-forms-serverless-gatsby-netlify">here</a></div>
             <div>You can check the code repo <a href="https://github.com/snipcart/gatsby-netlify">here</a></div>
